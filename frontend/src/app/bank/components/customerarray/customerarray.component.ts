e5b276a43c62fc342989f45dd2b9d51afe813e79
username <wecp-dummy@gmail.com>
2024-07-26 12:49:21 +0000

    DAY19

--- frontend/src/app/bank/components/customerarray/customerarray.component.ts
+++ frontend/src/app/bank/components/customerarray/customerarray.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { of } from 'rxjs';
-import { Customer } from '../../types/Customer';
+// import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+//import { of } from 'rxjs';
+//import { Customer } from '../../types/Customer';
+import { CustomerTS } from '../../types/tstypes/Customerts';
 
 @Component({
   selector: 'app-customerarray',
-  standalone: true,
-  imports: [],
+  // standalone: true,
+  // imports: [],
   templateUrl: './customerarray.component.html',
-  styleUrl: './customerarray.component.css'
+  styleUrls:[ './customerarray.component.css']
 })
 export class CustomerarrayComponent {
-  
+  customers: CustomerTS[] =[
+    new CustomerTS("rohgan","a@g.com","aj","a12","IT","2")
+  ]
 }
